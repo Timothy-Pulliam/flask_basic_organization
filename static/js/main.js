9714d0982ef15224d8a8fc0a0f9ff8829e2053f0
Timothy Pulliam <timothy.dpulliam@gmail.com>
2019-04-28 20:50:43 -0400

    Fixed codebase for static files

---
+++ static/js/main.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+  el: '#app',
+  delimiters: ['[[', ']]'],
+  data: {
+    product: 'Socks',
+    description: "100% Cotton Men's socks. Breathable for odor resistence.",
+    altText: "A pair of socks",
+    link: "https://bombas.com/collections/mens-socks",
+    image: "/static/img/vmSocks-green.jpg",
+    inventory: 8,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        viariantId: 2234,
+        variantColor: "green",
+        variantImage: "/static/img/vmSocks-green.jpg",
+      },
+      {
+        viariantId: 2235,
+        variantColor: "blue",
+        variantImage: "/static/img/vmSocks-blue.jpg",
+      }
+    ],
+    sizes: ['small', 'medium', 'large',],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    },
+  }
+})
